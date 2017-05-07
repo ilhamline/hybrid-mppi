b62a412d45f908bc572c1e10e65fcc7efd011ed3
Ilham Kurniawan <ilhamkurni@gmail.com>
2017-05-07 22:26:44 +0700

    Add dynamic sorting feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { HomePage, SortingPage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,6 +17,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    SortingPage,
     TabsPage
   ],
   imports: [
@@ -29,6 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    SortingPage,
     TabsPage
   ],
   providers: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'sorting.html',
 })
-export class HomePage {
-
+export class SortingPage {
+  item;
+  form = { size: '' };
   public tap: number = 0;
-  public jumlahData: number = 10000;
+  public size: number;
   public hehe: string;
   public startTime: string;
   public endTime: string;
   public elapsedTime: string;
-  constructor(public navCtrl: NavController) {
+
+  constructor(params: NavParams) {
+    this.item = params.data.item;
+  }
+
+  startSort() {
+    this.size = parseInt(this.form.size);
+    switch (this.item.type) {
+      case 'bubbleSort':
+        this.bubbleSort();
+        break;
+      case 'quickSort':
+        this.quickSort();
+        break;
+      case 'nQueens':
+        this.nQueens();
+        break;
+    }
+  }
+
+  bubbleSort() {
 
   }
 
-  tapEvent(e) {
+  quickSort() {
     var arr = [];
-    for (var i=0, t=this.jumlahData; i<t; i++) {
+    for (var i=0, t=this.size; i<t; i++) {
       arr.push(Math.round(Math.random() * t))
     }
     var sTime = Date.now();
@@ -28,7 +48,61 @@ export class HomePage {
     this.startTime = new Date(sTime).toString();
     this.endTime = new Date(eTime).toString();
     this.elapsedTime = (eTime-sTime).toString();
-    this.hehe = arr.toString()
+    if (this.size < 100000) {
+      this.hehe = arr.toString()
+    }
+  }
+
+  nQueens() {
+
+  }
+}
+
+@Component({
+  template: `
+            <ion-header>
+              <ion-navbar>
+                <ion-title>Home</ion-title>
+              </ion-navbar>
+            </ion-header>
+            <ion-content>
+              <ion-list>
+                <button ion-item *ngFor="let item of items" (click)="openSortingPage(item)" icon-left>
+                  <ion-icon [name]="'logo-' + item.icon" [ngStyle]="{'color': item.color}" item-left></ion-icon>
+                  {{ item.title }}
+                </button>
+              </ion-list>
+            </ion-content>
+            `
+})
+export class HomePage {
+  items = [];
+
+  constructor(public nav: NavController) {
+    this.items = [
+      {
+        'title': 'Bubble sort',
+        'icon': 'angular',
+        'type': 'bubbleSort',
+        'color': '#E63135'
+      },
+      {
+        'title': 'Quick sort',
+        'icon': 'css3',
+        'type': 'quickSort',
+        'color': '#0CA9EA'
+      },
+      {
+        'title': 'N queens problem',
+        'icon': 'css3',
+        'type': 'nQueens',
+        'color': '#0CA9EA'
+      },
+    ]
+  }
+
+  openSortingPage(item) {
+    this.nav.push(SortingPage, { item: item });
   }
 
 }
